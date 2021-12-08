9a12ceea84977c55ca934b05c370189bd9ab137b
akshaythalmi <92512151+akshaythalmi@users.noreply.github.com>
2021-12-08 11:23:14 +0530

    Add files via upload

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+
+@Module({
+  imports: [ProductsModule, MongooseModule.forRoot('mongodb+srv://mongoUser:wc4S1NLpVHrsPESQ@cluster0.tvtuj.mongodb.net/nest-mongodb?retryWrites=true&w=majority')],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
